2fa9a2879eb34a082e5a954d17c5ab05e3bcbdca
Md Saif Ali Ansari <71248588+spellsaif@users.noreply.github.com>
2023-07-31 23:46:39 +0000

    added logo

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { Home, Settings, PlusCircle } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import {usePathname} from 'next/navigation'
 
@@ -28,11 +29,14 @@ const Sidebar = () => {
         }
     ]
     return ( 
-        <section className="bg-secondary/50 w-24 py-4">
-            <div className="w-full flex flex-col items-center">
+        <section className="bg-secondary/50 w-24 py-6 flex flex-col items-center">
+            <Link href="/">
+                <Image src="/logo.png" alt="logo" width={60} height={60} />
+            </Link>
+            <div className="w-full flex flex-col items-center mt-16">
                 {
                 routes.map(({href, label, icon: Icon}) => (
-                    <Link key={href} href={href} className={cn("flex flex-col items-center rounded mt-2 w-20  py-2 hover:bg-primary/10", pathname === href ? "bg-primary/10": "")}>
+                    <Link key={href} href={href} className={cn("flex flex-col items-center rounded mt-4 w-20  py-2 hover:bg-primary/10", pathname === href ? "bg-primary/10": "")}>
                         <Icon />
                         {label}
                     </Link>
